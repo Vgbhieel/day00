7228036904bc422063bb0c01f7b504c1f30d1df5
Vitor Nascimento <bielvitor2008@hotmail.com>
2023-08-01 14:51:24 -0300

    feat: get changes from ex00

---
+++ ex01/lib/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:ex01/home_page.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
